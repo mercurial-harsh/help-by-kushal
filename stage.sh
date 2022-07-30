00513dbbeb98f3b82810f515f6834fce7eb44acb
root <root@vaishali-dev.us-east4-c.c.sally-355121.internal>
2022-07-30 15:59:51 +0000

    stahing changes

---
+++ stage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+echo "\nBuilding contnainer\n"
+docker build -t staging .
+sleep 15;
+echo "\nMaking the build directory"
+docker run --mount type=bind,source="$(pwd)",target=/app staging sh -c "npm run build"
